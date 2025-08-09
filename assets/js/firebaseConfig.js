210bdb98ec381e18b9720240287e0c46e023aff4
asp-web1 <aspmail100@gmail.com>
2025-08-09 12:05:47 +0530

    Add Firebase configuration file

---
+++ assets/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpfA8ErTJdibvnuwLJBjk5jGg2V4a0UQQ",
+  authDomain: "asp1db.firebaseapp.com",
+  databaseURL: "https://asp1db-default-rtdb.firebaseio.com",
+  projectId: "asp1db",
+  storageBucket: "asp1db.firebasestorage.app",
+  messagingSenderId: "39698895805",
+  appId: "1:39698895805:web:830e71e698cf26ffe0b8d4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
